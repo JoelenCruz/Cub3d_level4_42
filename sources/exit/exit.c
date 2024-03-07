80e332dd5369766dede6b739c06c83383800c8b7
joe <joelencruz@gmail.com>
2024-03-07 15:17:58 +0000

    added comments in functions

--- sources/exit/exit.c
+++ sources/exit/exit.c
@@ -6,7 +6,7 @@
 /*   By: joe <joe@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 12:20:16 by joe               #+#    #+#             */
-/*   Updated: 2024/03/06 13:33:24 by joe              ###   ########.fr       */
+/*   Updated: 2024/03/07 15:16:43 by joe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@
 // 		close (lower++);
 // }
 
+/**
+ * @brief print a message and exit the program
+ * 
+ * @param msg 
+ * @param code 
+ */
 static void	print_exit_message(const char *msg, const int code)
 {
 	if (code)
@@ -28,12 +34,24 @@ static void	print_exit_message(const char *msg, const int code)
 		ft_putendl_fd (EXIT_MSG, 1);
 }
 
+/**
+ * @brief Close the program
+ * 
+ * @param cub 
+ * @return int 
+ */
 int	cub_close(t_cub *cub)
 {
 	cub_exit (cub, NULL, 0);
 	return (0);
 }
 
+/**
+ * @brief Exits the program with a message
+ * @param cub 
+ * @param msg 
+ * @param code 
+ */
 void	cub_exit(t_cub *cub, const char *msg, const int code)
 {
 	print_exit_message(msg, code);
--- sources/init/init.c
+++ sources/init/init.c
@@ -6,34 +6,52 @@
 /*   By: joe <joe@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 11:57:55 by joe               #+#    #+#             */
-/*   Updated: 2024/03/06 13:48:11 by joe              ###   ########.fr       */
+/*   Updated: 2024/03/07 15:17:23 by joe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-
+/**
+ * @brief Initializes the struct cub with zeros
+ * @param cub 
+ */
 static void	cub_bzero(t_cub *cub)
 {
 	ft_bzero(cub, sizeof (t_cub));
 }
 
+/**
+ * @brief Exits the program with a message
+ * @param cub 
+ * @param msg 
+ * @param status 
+ */
 static void	check_argcs(int argc, t_cub *cub)
 {
 	if (argc != 2)
 		cub_exit(cub, ERROR_ARGS, 1);
 }
 
-
+/**
+ * @brief verify if the file extension is ".cub"
+ * 
+ * @param map_name 
+ * @param cub 
+ */
 static void verify_path(char *map_name, t_cub *cub)
 {
-    cub -> path_maps = map_name; //salvar o nome do arquivo na struct
-    
-    // Verifica se o nome do arquivo termina com ".cub"
+    cub -> path_maps = map_name;
     if (ft_strncmp(map_name + ft_strlen(map_name) - 4, ".cub", 4) != 0)
         cub_exit(cub, ERROR_EXTETION, 1);
 }
 
+/**
+ * @brief Initializes the struct cub
+ * @param argc 
+ * @param argv 
+ * @param cub 
+ */
 void	cub_init(int argc, char *argv[], t_cub *cub)
 {
 	cub_bzero(cub);
