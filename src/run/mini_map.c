c3f5199676cbbdda09085b8bb62845bff0b98bed
joe <joelencruz@gmail.com>
2024-06-12 16:54:18 +0100

    minimapa

--- src/run/mini_map.c
+++ src/run/mini_map.c
@@ -6,56 +6,70 @@
 /*   By: joe <joe@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 19:28:37 by joe               #+#    #+#             */
-/*   Updated: 2024/06/09 20:00:35 by joe              ###   ########.fr       */
+/*   Updated: 2024/06/12 16:50:46 by joe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "cub3d.h"
 
-void draw_rectangle_2(void *mlx, void *win, int x, int y, int width, int height, int color)
+#define MINIMAP_SCALE 10 // Nova escala para o minimapa
+
+void set_pos(t_coord *pos, int x, int y)
 {
-    int i, j;
-    for (i = x; i < x + width; i++)
+    pos->x = x;
+    pos->y = y;
+}
+
+static int case_color(t_cub *cub, int x, int y)
+{
+    char c;
+
+    c = cub->scene_map[y][x];
+    if (c == '1' || c == '2')
+        return (0x0);
+    else if (y == (int)cub->p.y && x == (int)cub->p.x)
+        return (0x10CC10);
+    else if (c == 'A')
+        return (0xCCCCCC);
+    return (0xFFFFFF);
+}
+
+static void draw_minimap(t_cub *cub, t_coord *start, t_coord *end)
+{
+    int i;
+    int j;
+    int color;
+
+    i = 0;
+    while (i < cub->map_height)
     {
-        for (j = y; j < y + height; j++)
+        j = 0;
+        while (j < cub->map_width)
         {
-            mlx_pixel_put(mlx, win, i, j, color);
+            color = case_color(cub, j, i);
+            if (color >= 0)
+            {
+                set_pos(start, WIN_WIDTH - (cub->map_width * MINIMAP_SCALE) - 5 + (j * MINIMAP_SCALE),
+                        WIN_HEIGHT - (cub->map_height * MINIMAP_SCALE) - 5 + (i * MINIMAP_SCALE));
+                set_pos(end, WIN_WIDTH - (cub->map_width * MINIMAP_SCALE) + (j * MINIMAP_SCALE),
+                        WIN_HEIGHT - (cub->map_height * MINIMAP_SCALE) + (i * MINIMAP_SCALE));
+                draw_rectangle(cub, *start, *end, color);
+            }
+            j++;
         }
+        i++;
     }
 }
 
-
-void render_mini_map(t_cub *cub)
+void 	render_mini_map (t_cub *cub)
 {
-    // Defina o tamanho e a posição da janela do mini-mapa
-    int mini_map_width = 200;
-    int mini_map_height = 200;
-    int mini_map_x = WIN_WIDTH - mini_map_width - 10;
-    int mini_map_y = 10;
+    t_coord start;
+    t_coord end;
 
-    // Desenhe a moldura da janela do mini-mapa
-    draw_rectangle_2(cub->mlx, cub->win, mini_map_x, mini_map_y, mini_map_x + mini_map_width, mini_map_y + mini_map_height, 0xFFFFFF);
-
-    // Determine a escala do mini-mapa
-    float scale_x = mini_map_width / (float)cub->map_width;
-    float scale_y = mini_map_height / (float)cub->map_height;
-
-    // Renderize o mapa de localização dentro da janela do mini-mapa
-    for (int y = 0; y < cub->map_height; y++)
-    {
-        for (int x = 0; x < cub->map_width; x++)
-        {
-            int cell_x = mini_map_x + x * scale_x;
-            int cell_y = mini_map_y + y * scale_y;
-            int color = (cub->scene_map[y][x] == '1') ? 0xFFFFFF : 0x000000; // Define a cor com base no conteúdo do mapa
-            draw_rectangle_2(cub->mlx, cub->win, cell_x, cell_y, cell_x + scale_x, cell_y + scale_y, color);
-        }
-    }
-
-    // Desenhe o jogador como um ponto no mini-mapa
-    int player_mini_map_x = mini_map_x + cub->p.x * scale_x;
-    int player_mini_map_y = mini_map_y + cub->p.y * scale_y;
-    mlx_pixel_put(cub->mlx, cub->win, player_mini_map_x, player_mini_map_y, 0xFF0000);
+    set_pos(&start, 2, WIN_HEIGHT - 27);
+    set_pos(&end, 210, WIN_HEIGHT - 2);
+    draw_rectangle(cub, start, end, 0xFFFFFF);
+    draw_minimap(cub, &start, &end);
 }
 
